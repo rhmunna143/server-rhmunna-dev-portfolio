0e0df3580ca548cac263a553e890bc3a542a43f1
rhmunna143 <rhmunna19@gmail.com>
2024-01-07 23:36:22 +0600

    Database connected

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-const app = express()
+require('dotenv').config();
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 //middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('I am walking...!')
 })
+
+
+
+// MongoDB driver
+const uri = process.env.URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
